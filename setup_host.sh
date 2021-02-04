6637d3f361915f2ba0d0e8b4cba499b49fa37696
Giovanni Toffetti <toff@zhaw.ch>
2021-02-04 11:37:33 +0000

    Making sure lightdm starts

--- setup_host.sh
+++ setup_host.sh
@@ -8,8 +8,9 @@ fi
 echo "Copying xorg.conf to X config dir"
 cp setup_host/xorg.conf /etc/X11
 echo "Installing lightdm"
-exec setup_host/basics.sh
+apt remove -y gdm3
+setup_host/basics.sh
 echo "Starting lightdm"
-systemctl start --wait display-manager.service
+systemctl start display-manager.service
 echo "Adding users to lightdm xhost"
-exec setup_host/add_xhost.sh
+setup_host/add_xhost.sh
--- setup_host/basics.sh
+++ setup_host/basics.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 apt-get update -y
-apt-get install -y --no-install-recommends net-tools lightdm xvfb #lubuntu-desktop
+apt-get install -y net-tools lightdm xvfb lightdm-autologin-greeter #lubuntu-desktop
